abbcdd6676320815a8656793e3ce398dc8e83882
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-04 13:11:04 +0530

    Login component design completed

---
+++ src/Components/Login.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+    const [inputField,setInputField]=useState(
+        {username:"",password:""}
+    )
+
+    const inputHandler=(event)=>{
+        setInputField({...inputField,[event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        console.log(inputField)
+    }
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <h1>Login Page</h1>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Username</label>
+                            <input onChange={inputHandler} type="text" className="form-control" name="username" value={inputField.username} />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Password</label>
+                            <input onChange={inputHandler} type="password" name="password" id="" className="form-control" value={inputField.password} />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button onClick={readValue} className="btn btn-primary">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
