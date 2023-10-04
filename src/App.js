ce7371a04d87d5e506e94a075fd6c9258ffa1e9e
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-04 22:59:50 +0530

    SearchUser component completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './Components/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddUser from './Components/AddUser';
+import SearchUser from './Components/SearchUser';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
     <Route path="/" exact element={<Login/>} />
     <Route path="/a" exact element={<AddUser/>}/>
+    <Route path="/s" exact element={<SearchUser/>} />
     </Routes>
     </BrowserRouter>
     </div>
