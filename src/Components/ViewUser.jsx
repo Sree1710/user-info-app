06ff80ee3f8444574dc5ff8812b2a5bd173b12d4
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-05 08:43:12 +0530

    ViewUser component completed

---
+++ src/Components/ViewUser.jsx
@@ -0,0 +1,54 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const ViewUser = () => {
+    const [userData, setUserData] = useState(
+        { "data": [] }
+    )
+
+    const apiLink = "https://reqres.in/api/users?page=1"
+
+    const [isLoading, setIsLoading] = useState(true)
+
+    const getData = () => {
+        axios.get(apiLink).then(
+            (Response) => {
+                setUserData(Response.data)
+                setIsLoading(false)
+            }
+        )
+    }
+
+    useEffect(() => { getData() }, [])
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <h1>View User</h1>
+                        </div>
+                        {isLoading ? <p>Loading...</p> : <div className="row g-3">
+                            {userData.data.map(
+                                (value, index) => {
+                                    return <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                        <div class="card">
+                                            <img src={value.avatar} class="card-img-top" alt="..." />
+                                            <div class="card-body">
+                                                <h5 class="card-title">{value.first_name},{value.last_name}</h5>
+                                                <p class="card-text">{value.id}</p>
+                                                <p class="card-text">{value.email}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                            )}
+                        </div>}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewUser
